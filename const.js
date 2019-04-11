a09b54149896f9692481750e6d88210bf96dd047
aukoonpang <223520133@qq.com>
2019-04-11 13:39:32 +0800

    提交后台代码

---
+++ const.js
@@ -0,0 +1,32 @@
+const resUrl = 'http://192.168.31.230:8090'
+
+const category = [
+    'Biomedicine',
+    'BusinessandManagement',
+    'ComputerScience',
+    'EarthSciences',
+    'Economics',
+    'Engineering',
+    'Education',
+    'Environment',
+    'Geography',
+    'History',
+    'Laws',
+    'LifeSciences',
+    'Literature',
+    'SocialSciences',
+    'MaterialsScience',
+    'Mathematics',
+    'MedicineAndPublicHealth',
+    'Philosophy',
+    'Physics',
+    'PoliticalScienceAndInternationalRelations',
+    'Psychology',
+    'Statistics'
+]
+
+
+module.exports = {
+    resUrl,
+    category
+}
