c72ed1e45e532aa14f580e2210d4feb0b0860270
aukoonpang <223520133@qq.com>
2019-04-20 02:51:44 +0800

    修复在线语音保存路径错误

--- voice.js
+++ voice.js
@@ -68,6 +68,8 @@ function createVoice(req, res) {
                 res.send(mp3)
             } else {
                 const fileName = new Date().getTime()
+                //windows下路径
+                //const filePath = `${mp3FilePath}\\${fileName}.mp3`
                 const filePath = `${mp3FilePath}/${fileName}.mp3`
                 const downloadUrl = `${resUrl}/mp3/${fileName}.mp3`
                 fs.writeFile(filePath, mp3, 'binary', err => {
